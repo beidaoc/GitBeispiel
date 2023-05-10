c6847262407e0726d2bdbb563b0c199241470184
张涛 <zhangtao@HUAWEI-MateBook-Pro.local>
2023-05-10 16:54:18 +0200

    Summe wurde implementiert

--- Main.java
+++ Main.java
@@ -7,27 +7,11 @@
  */
 public class Main
 {
-    // 实例变量 - 用你自己的变量替换下面的例子
-    private int x;
-
-    /**
-     * 类 Main 的对象的构造函数
-     */
-    public Main()
-    {
-        // 初始化实例变量
-        x = 0;
+    public static void main(String args[]){
+        int summe = 0;
+        for (int i = 0; i <= 10; i++ ){
+            summe = summe + 1;
+        }
     }
-
-    /**
-     * 一个方法的例子 - 使用你自己的说明替代它
-     * 
-     * @参数 y，方法的一个样本参数
-     * @返回 x，y的和 
-     */
-    public int sampleMethod(int y)
-    {
-        // 在这里加入你的代码
-        return x + y;
-    }
+   
 }
